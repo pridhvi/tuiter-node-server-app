4b35c6cc635d5d199178c007fef27401803ea7d3
pridhvi <pridhvimuthuraju@gmail.com>
2023-03-26 21:20:31 -0400

    initial commit

---
+++ controllers/users/users-controller.js
@@ -0,0 +1,20 @@
+import people from './users.js'
+let users = people
+
+const UserController = (app) => {
+    app.get('/api/users', findUsers)
+}
+
+const findUsers = (req, res) => {
+    const type = req.query.type
+    if (type) {
+        const usersOfType = users
+            .filter(u => u.type === type)
+        res.json(usersOfType)
+        return
+    }
+
+    res.json(users)
+}
+
+export default UserController
